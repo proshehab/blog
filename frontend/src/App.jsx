79861ca67fbbad5ca510de5ac9e0f9e5594f18fa
proshehab <shehab.shakib@gmail.com>
2025-10-15 19:31:38 +0600

    mern blog upload

---
+++ frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import { BrowserRouter, Routes } from 'react-router-dom'
+
+
+function App() {
+ 
+
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
