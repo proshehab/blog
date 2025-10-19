a1b5f78f7e49ef22e373724f0fb1f39f1239abe8
proshehab <shehab.shakib@gmail.com>
2025-10-19 14:49:05 +0600

    update

--- server/routes/api.php
+++ server/routes/api.php
@@ -4,7 +4,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\AuthController;
 
-Route::post('/admin/login',[AuthController::class,'authenticate']);
+Route::post('/admin/login', [AuthController::class, 'authenticate']);
+Route::post('/admin/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 
 Route::get('/user', function (Request $request) {
     return $request->user();
