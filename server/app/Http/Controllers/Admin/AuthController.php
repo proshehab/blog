f5cbdebd6f26964ed15905b4c28737be6b086953
backupfiles4466 <backup.files4466@gmail.com>
2025-10-19 13:23:25 +0600

    server auth update

--- server/app/Http/Controllers/Admin/AuthController.php
+++ server/app/Http/Controllers/Admin/AuthController.php
@@ -5,29 +5,52 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+use App\Models\User;
 
 class AuthController extends Controller
 {
     public function authenticate(Request $request)
     {
-        $validated = $request->validate([
+        $validator = Validator::make($request->all(), [
             'email' => 'required|email',
             'password' => 'required|string',
         ]);
 
-        if ($validated->fails()) {
+        if ($validator->fails()) {
             return response()->json([
                 'status' => 400,
-                'errors' => $validated->errors()
+                'errors' => $validator->errors()
             ], 400);
         }
 
-        if (Auth::attempt($validated)) {
-            $request->session()->regenerate();
-            return response()->json(['message' => 'Authenticated successfully'], 200);
+        if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
+            $user = User::find(Auth::user()->id);
+
+            if ($user->role === 'admin') {
+                $token = $user->createToken('token')->plainTextToken;
+
+                return response()->json([
+                    'status' => 200,
+                    'token' => $token,
+                    'id' => $user->id,
+                    'name' => $user->name,
+                    
+                ], 200);
+            } else {
+                return response()->json([
+                    'status' => 4001,
+                    'message' => 'You do not have admin access.'
+                ], 401);
+
+            }
+        } else {
+            return response()->json([
+                'status' => 401,
+                'message' => 'Either email or password is incorrect.'
+            ], 401);
         }
 
-
-        return response()->json(['message' => 'Authentication failed'], 401);
+        
     }
 }
