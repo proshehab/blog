07adf511a3ddfa4181d3238e56df8fb7bcc2e125
proshehab <shehab.shakib@gmail.com>
2025-10-20 10:55:42 +0600

    update

--- frontend/src/components/pages/Login.jsx
+++ frontend/src/components/pages/Login.jsx
@@ -10,8 +10,14 @@ const Login = () => {
     formState: { errors },
   } = useForm()
 
+  const onSubmit = (data) => {
+    console.log(data)
+  }
+
   return (
+           
     <div className="flex min-h-screen items-center justify-center bg-gray-100">
+         <form onSubmit={handleSubmit(onSubmit)}>
             <div className="w-full max-w-md bg-white rounded-2xl shadow-lg p-8">
                 <h2 className="text-2xl font-bold text-center mb-6">Login</h2>
                 <form className="space-y-4">
@@ -23,9 +29,15 @@ const Login = () => {
                             Email
                         </label>
                         <input
+                        {
+                            ...register("email", { required: true })
+                        }
                             placeholder="Enter Email"
                             type="text"
                             className={`focus:outline-none w-full border border-gray-200 py-2 px-4 rounded-lg`}
+                            {
+                                ...register("email", { required: true })
+                            }
                         />
                     </div>
                     <div>
@@ -55,7 +67,9 @@ const Login = () => {
                     </Link>
                 </p>
             </div>
+           </form>
         </div>
+         
   )
 }
 
