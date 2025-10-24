cca691ba4d6a89cdea6a4fad53b928a33fff07fa
proshehab <shehab.shakib@gmail.com>
2025-10-24 17:58:45 +0600

    layoute update

--- front/src/components/common/Layout.jsx
+++ front/src/components/common/Layout.jsx
@@ -1,19 +1,7 @@
-<<<<<<< HEAD
 import React from 'react'
 
-const Layout = () => {
+export const Layout = () => {
   return (
     <div>Layout</div>
   )
 }
-
-export default Layout
-=======
-import React from "react";
-
-const Layout = () => {
-  return <div></div>;
-};
-
-export default Layout;
->>>>>>> 978a45f29d6569a7f96da7b9d471c206ee09f131
